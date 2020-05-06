e9e5814a61fd36e030f12beda1132e8a5bc8302d
Richard Cho <96richardohc@gmail.com>
2020-05-06 00:37:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if 
+  if phrase == phrase.upcase
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
